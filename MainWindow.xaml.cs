f65390d4a5d5e1a8ca974cdc09e31ac97f59f365
godspeed-ai <125993906+godspeed-ai@users.noreply.github.com>
2023-03-22 22:22:44 +0800

    更新公尺除錯
    公尺除錯更新

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -52,14 +52,23 @@ namespace LengthCalculator
 
         private void txtM_KeyUp(object sender, KeyEventArgs e)
         {
-            double douM;
-            douM = Convert.ToDouble(txtM.Text);
+            strInput = txtM.Text;
 
-            txtCM.Text = string.Format("{0:0.##########}", douM * 100);
-            txtKM.Text = string.Format("{0:0.##########}", douM * 0.001);
-            txtIn.Text = string.Format("{0:0.##########}", douM * 39.37);
-            txtFt.Text = string.Format("{0:0.##########}", douM * 3.28);
-            txtYard.Text = string.Format("{0:0.##########}", douM * 1.09);
+
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                txtCM.Text = string.Format("{0:0.##########}", douOutput * 100); 
+                txtKM.Text = string.Format("{0:0.##########}", douOutput * 0.001);
+                txtIn.Text = string.Format("{0:0.##########}", douOutput * 39.37);
+                txtFt.Text = string.Format("{0:0.##########}", douOutput * 3.28);
+                txtYard.Text = string.Format("{0:0.##########}", douOutput * 1.09);
+            }
+            else
+            {
+                // 如果無法轉型，則是在說明文字中顯示錯誤訊息，並且將txtCM文字框清除
+                txtInfo.Text = "請輸入數字";
+                txtM.Text = "";
+            }
         }
 
         private void txtKM_KeyUp(object sender, KeyEventArgs e)
